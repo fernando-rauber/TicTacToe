c0d953c69cb1cbe70aa56234a1246a546df55b02
fernandocristiano <75613223+fernandocristiano@users.noreply.github.com>
2022-10-07 15:54:45 +0100

    App background

--- app/src/main/java/uk/fernando/tictactoe/activity/MainActivity.kt
+++ app/src/main/java/uk/fernando/tictactoe/activity/MainActivity.kt
@@ -6,12 +6,18 @@ import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Brush
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import uk.fernando.tictactoe.navigation.Directions
 import uk.fernando.tictactoe.navigation.buildGraph
 import uk.fernando.tictactoe.theme.TicTacToeTheme
+import uk.fernando.tictactoe.theme.green
+import uk.fernando.tictactoe.theme.greenDark
+import uk.fernando.util.component.UpdateStatusBar
 
 @OptIn(ExperimentalAnimationApi::class)
 class MainActivity : ComponentActivity() {
@@ -21,11 +27,21 @@ class MainActivity : ComponentActivity() {
         setContent {
             val controller = rememberNavController()
 
-//            UpdateStatusBar(color = dark)
+            UpdateStatusBar(greenDark)
 
             TicTacToeTheme {
 
-                Box() {
+                Box {
+                    // Background
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .scale(1.5f)
+                            .background(Brush.radialGradient(colors = listOf(green.copy(.8f), green, greenDark))),
+                        content = {}
+                    )
+
+                    // Content
                     NavHost(
                         navController = controller,
                         startDestination = Directions.home.path
--- app/src/main/java/uk/fernando/tictactoe/theme/Color.kt
+++ app/src/main/java/uk/fernando/tictactoe/theme/Color.kt
@@ -2,17 +2,11 @@ package uk.fernando.tictactoe.theme
 
 import androidx.compose.ui.graphics.Color
 
-val pastelBlue = Color(0xFFbad5f0)
-val pastelYellow = Color(0xFFf2e9cc)
-val pastelRed = Color(0xFFFFB3B3)
-val pastelGreenDark = Color(0xFFa5d5d5)
-val pastelGreen = Color(0xFFc2d5a8)
-val pastelPurple = Color(0xFFb0abcb)
-val pastelPink = Color(0xFFf3b1cd)
-val pastelOrange = Color(0xFFFBC6A4)
+val green = Color(0xFF169D77)
+val greenDark = Color(0xFF107055)
 
-val green = Color(0xFF028587)
-val greenDark = Color(0xFF005C5F)
+
+
 val gold = Color(0xFFFFD700)
 val orange = Color(0xFFff7440)
 val red = Color(0xFFd94443)
